066fa27560f0a95b0bdc13a25a2eb35eaa8c2bf3
sengwoong <92924243+sengwoong@users.noreply.github.com>
2024-01-03 14:07:52 +0900

    :sparkles: Feat: 하드코드 체인지
    solid 를 지키며
    프로바이더 패턴을 지키게 바꾸었습니다.

--- src/context/OpenModalProvider.jsx
+++ src/context/OpenModalProvider.jsx
@@ -1,5 +1,5 @@
 // authContext.jsx
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
 
 import LoginForm from "../components/Login/LoginForm";
 import AlertForm from "../components/Login/AlertForm";
@@ -9,37 +9,53 @@ import LoginRegister from "../components/Login/LoginRegister.jsx";
 import AlertFormMain from  "../components/Login/AlertFormMain";
 import ProfleForm from "../components/Login/ProfleForm.jsx";
 
+// Actions
+const OPEN_FORM = "OPEN_FORM";
+const CLOSE_FORM = "CLOSE_FORM";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case OPEN_FORM:
+      return { ...state, selectedCategory: action.payload };
+    case CLOSE_FORM:
+      return { ...state, selectedCategory: null };
+    default:
+      return state;
+  }
+};
+
 export const OpenModalContext = createContext();
 
 export const OpenModalProvider = ({ children }) => {
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const initialState = { selectedCategory: null }; // Set the initial state to null
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const openForm = (category) => {
-    setSelectedCategory(category);
+    dispatch({ type: OPEN_FORM, payload: category });
   };
 
   const closeForm = () => {
-    setSelectedCategory(null);
+    dispatch({ type: CLOSE_FORM });
   };
 
+
+  const formConfig = {
+    loginForm: LoginForm,
+    registerForm: LoginRegister,
+    alertForm: AlertForm,
+    notifyForm: NotifyForm,
+    alertFormMain: AlertFormMain,
+    passwordChangeForm: PasswordChangeForm,
+    profileForm: ProfleForm,
+  };
+  
   const renderForm = () => {
-    switch (selectedCategory) {
-      case "loginForm":
-        return <LoginForm  />;
-      case "registerForm":
-        return <LoginRegister />;
-      case "alertForm":
-        return <AlertForm onClose={closeForm} />;
-      case "notifyForm":
-        return <NotifyForm onClose={closeForm} />;
-      case "alertFormMain":
-        return <AlertFormMain></AlertFormMain>
-      case "passwordChangeForm":
-        return <PasswordChangeForm onClose={closeForm} />;
-        case "profleForm":
-        return <ProfleForm />;
-      default:
-        return null;
+    const FormComponent = formConfig[state.selectedCategory];
+  
+    if (FormComponent) {
+      return <FormComponent onClose={closeForm} />;
+    } else {
+      return null;
     }
   };
 
